d16ab8bb5a4859ee832183a333aab7c844e88b60
aboynejames <james@aboynejames.co.uk>
2017-02-28 15:32:23 +0000

    created sperate sending either via browser example

---
+++ javascriptapi/smartcontracts/multiple.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.0;
+
+/*
+* test smart contract a simple multiplication function
+*/
+contract test {
+
+    function test()
+    {
+      // constructor
+    }
+
+   function multiply(uint a) constant returns(uint d) {
+       return a * 7;
+   }
+
+};
